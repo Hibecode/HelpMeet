7e99a392f7eb6a347a22a744453c8c8952cf9476
Hibecode <ibkokunoye@gmail.com>
2022-08-10 14:31:38 +0100

    Add retrofit classes and dependencies

---
+++ app/src/main/java/com/example/helpmeet/api/HelpMeetApi.kt
@@ -0,0 +1,10 @@
+package com.example.helpmeet.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface HelpMeetApi {
+
+    /*@GET("/us/rss/topalbums/limit=100/json")
+    suspend fun getTopAlbums(): Response<TopAlbums>*/
+}
---
+++ app/src/main/java/com/example/helpmeet/api/RetrofitInstance.kt
@@ -0,0 +1,35 @@
+package com.example.helpmeet.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+
+        private const val BASE_URL = "https://itunes.apple.com"
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+
+        /*val api: HelpMeetApi by lazy {
+            retrofit.create(AlbumApi::class.java)
+        }*/
+
+    }
+}
