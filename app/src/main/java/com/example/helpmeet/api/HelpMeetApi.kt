0ac7d12a1181355bafa75559040ca185fafbb504
Hibecode <ibkokunoye@gmail.com>
2022-08-13 12:16:23 +0100

    Add endpoint complete

--- app/src/main/java/com/example/helpmeet/api/HelpMeetApi.kt
+++ app/src/main/java/com/example/helpmeet/api/HelpMeetApi.kt
@@ -2,10 +2,13 @@ package com.example.helpmeet.api
 
 import com.example.helpmeet.models.Estate
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface HelpMeetApi {
 
     @POST("/api/v1/estate/registration")
-    suspend fun registerUser(): Response<Estate>
+    suspend fun registerUser(
+        @Body estateRegData: Estate
+    ): Response<Estate>
 }
