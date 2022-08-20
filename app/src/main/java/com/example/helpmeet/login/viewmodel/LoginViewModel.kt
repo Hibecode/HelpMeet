56986f55031251209277855e3e0c4756dda1c0c3
Hibecode <ibkokunoye@gmail.com>
2022-08-20 23:18:03 +0100

    Add internet connection code to view model

--- app/src/main/java/com/example/helpmeet/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/helpmeet/login/viewmodel/LoginViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.helpmeet.login.viewmodel
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -16,4 +19,20 @@ class LoginViewModel(): ViewModel() {
         val response = RetrofitInstance.api.registerEstate(estateReg)
         estateRegResponse.value = response
     }
+
+
+   /* private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<AlbumApplication>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }*/
 }
