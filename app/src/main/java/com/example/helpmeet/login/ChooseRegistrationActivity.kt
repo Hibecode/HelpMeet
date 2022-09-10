83357de24df1ca521a297298d1145ec0dde6bb47
Hibecode <ibkokunoye@gmail.com>
2022-09-10 03:15:17 +0100

    Add navigation to choose register buttons

--- app/src/main/java/com/example/helpmeet/login/ChooseRegistrationActivity.kt
+++ app/src/main/java/com/example/helpmeet/login/ChooseRegistrationActivity.kt
@@ -1,12 +1,36 @@
 package com.example.helpmeet.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import com.example.helpmeet.R
+import com.example.helpmeet.databinding.ActivityChooseRegistrationBinding
 
 class ChooseRegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChooseRegistrationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_registration)
+        binding = ActivityChooseRegistrationBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.chooseResidentButton.setOnClickListener {
+            val intent = Intent(this, ResidentRegistrationActivity::class.java).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "something")
+            }
+            startActivity(intent)
+        }
+
+        binding.chooseEstateButton.setOnClickListener{
+            val intent = Intent(this, EstateRegistrationActivity::class.java).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "something")
+            }
+            startActivity(intent)
+        }
+
     }
 }
